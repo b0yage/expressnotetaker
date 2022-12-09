0af75d86d49e00fd7c21dfd1d4c79df0d30bf8dc
b0yage <christivnvertiz@gmail.com>
2022-12-08 19:04:23 -0600

    2nd commit

---
+++ server.js
@@ -0,0 +1,14 @@
+
+// running express, application, fs, and port//
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const port = process.env.PORT || 3001
+
+//handling data parsing//
+
+
+
+
+
+
